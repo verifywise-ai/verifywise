30e2164c21f09ee2d91fb6333ec6318b94cf634b
Debora Serra <debora.r.serra@gmail.com>
2026-04-12 21:23:30 -0700

    fix: fix test lib and undo pipeline changes

--- Clients/vite.config.ts
+++ Clients/vite.config.ts
@@ -1,5 +1,5 @@
 import svgr from "@svgr/rollup";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vitest/config";
 import { version } from "../version.json";
