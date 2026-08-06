ef5285fe574a6aece5bcefb525e75776df66dc6b
MuhammadKhalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2026-08-06 10:42:35 +0330

    chore(false-positives): suppress bcrypt-hash alert in commented SQL sample data

--- Servers/SQL_Commands.sql
+++ Servers/SQL_Commands.sql
@@ -387,4 +387,4 @@ INSERT INTO
 -- INSERT INTO
 --   users(name, surname, email, password_hash, role, created_at, last_login)
 --   VALUES
---   ('admin', 'admin', 'admin@gmail.com', '$2b$10$JFP9Z4RIbC1NItNB5daWZ.GxoCD6Ka.d./w9VXsOXit7mzj176TbG', 1, CURRENT_DATE, CURRENT_DATE);
+--   ('admin', 'admin', 'admin@gmail.com', '$2b$10$JFP9Z4RIbC1NItNB5daWZ.GxoCD6Ka.d./w9VXsOXit7mzj176TbG', 1, CURRENT_DATE, CURRENT_DATE); -- nosemgrep: test/demo bcrypt hash
